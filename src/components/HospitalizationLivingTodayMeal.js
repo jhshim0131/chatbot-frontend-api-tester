b493fb6f187124236d1885f34500100eef669ab4
dante.27 <dante.27@kakaohealthcare.com>
2024-11-19 16:39:29 +0900

    fix : url changed to interface mock

--- src/components/HospitalizationLivingTodayMeal.js
+++ src/components/HospitalizationLivingTodayMeal.js
@@ -5,9 +5,14 @@ function HospitalizationLivingTodayMeal() {
     const [error, setError] = useState(null); // 에러 상태 저장
     const [loading, setLoading] = useState(true); // 로딩 상태 저장
 
+    const localTestUrl = 'http://localhost:8081/dfd/api/hsp/few/v1/hsptlz/lvng/meal?hspId=9&usrId=109&mealDate=2024-11-08'; // 로컬 테스트용 API URL
+    const devUrl = 'https://karechat-hsp-yumc-dev.kakaohealthcare.com/dfd/api/hsp/few/v1/hsptlz/lvng/meal?hspId=9&usrId=109&mealDate=2024-11-08';
+    const url = 'https://46a3e800-0549-4a73-af78-ff4b0960636d.mock.pstmn.io/dfd/api/if/his/H51';
+
     useEffect(() => {
         // 페이지 로드 시 API 호출
-        fetch('https://karechat-hsp-yumc-dev.kakaohealthcare.com/dfd/api/hsp/few/v1/hsptlz/lvng/meal?hspId=9&usrId=109&mealDate=2024-11-08') // API URL 변경
+
+        fetch(url) // API URL 변경
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch');
